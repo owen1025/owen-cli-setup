042c23ec61a0fbc8791a87394eeeaa94368b91af
owen <myartame@gmail.com>
2018-10-23 23:08:17 +0900

    [ADD] install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install a xcode-cli
+sudo xcode-select --install
+
+# Install a brew(mac package manager)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
